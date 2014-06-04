0ca5905f53fd1782f9b00da920409d58ff2e6477
Michael Abramowicz <mbabramo@yahoo.com>
2014-06-04 16:31:04 -0400

    set R8RCloud as startup

--- ClassLibrary1/Misc/AzureSetup.cs
+++ ClassLibrary1/Misc/AzureSetup.cs
@@ -24,7 +24,7 @@ namespace ClassLibrary1.Misc
         {
             SetConfigurationSettingPublisher();
             return RoleEnvironment.IsAvailable ? RoleEnvironment.GetConfigurationSettingValue(configName)
-                : "Data Source=PC2012;Initial Catalog=Norm0001;Integrated Security=True;Connect Timeout=300";
+                : "ERROR GETTING CONFIGURATION SETTING"; // "Data Source=PC2012;Initial Catalog=Norm0001;Integrated Security=True;Connect Timeout=300";
                      // : ConfigurationManager.AppSettings[configName];
         }
 
