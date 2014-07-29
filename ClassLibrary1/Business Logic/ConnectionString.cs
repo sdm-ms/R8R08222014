d68651439fc5a43a2ac24475e4323ab78f1d45a5
mbabramo <mbabramo@yahoo.com>
2014-07-29 08:41:10 -0400

    Adding ConnectionString.cs manually
    Not clear why github for windows did not autodetect it and add it.

---
+++ ClassLibrary1/Business Logic/ConnectionString.cs
@@ -0,0 +1,48 @@
+using ClassLibrary1.Nonmodel_Code;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Model
+{
+    public static class ConnectionString
+    {
+        private static void CheckHardCodedAppropriate()
+        {
+            if (!GetIR8RDataContext.UseRealDatabase)
+                throw new Exception("Internal error. We should use the hard coded connections only for testing against the real database.");
+            if (RoleEnvironment.IsAvailable)
+                throw new Exception("Internal error. We should not be using hard coded connections within the role environment. Load from config files instead.");
+        }
+
+        private static string HardCodedUserProfileDatabase()
+        {
+            CheckHardCodedAppropriate();
+
+            const string hardCoded = "Data Source=PC2012;Initial Catalog=Norm0006;Integrated Security=True;Connect Timeout=300";
+            return hardCoded;
+        }
+
+
+        public static string GetUserProfileDatabaseConnectionString()
+        {
+            return AzureSetup.GetConfigurationSetting("ApplicationServices") ?? HardCodedUserProfileDatabase();
+        }
+
+        private static string HardCodedNormalizedDatabase()
+        {
+            CheckHardCodedAppropriate();
+
+            const string hardCoded = "Data Source=PC2012;Initial Catalog=Norm0006;Integrated Security=True;Connect Timeout=300";
+            return hardCoded;
+        }
+
+        public static string GetR8RNormalizedDatabaseConnectionString()
+        {
+            return AzureSetup.GetConfigurationSetting("R8RConnectionString") ?? HardCodedNormalizedDatabase();
+        }
+    }
+}
