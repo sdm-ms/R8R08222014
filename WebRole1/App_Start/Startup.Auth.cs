4f0db53e8230d4fdfac9b654653789c04ff3e2ab
sdm_ms@hotmail.com <sdm_ms@hotmail.com>
2014-09-15 18:35:59 +0530

    ApplyConditionsForClientIds

--- WebRole1/App_Start/Startup.Auth.cs
+++ WebRole1/App_Start/Startup.Auth.cs
@@ -28,53 +28,60 @@ namespace WebRole1
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-            //app.UseGoogleAuthentication(clientId: "1072934350544-dl5fr7svaedmn33tms38a89kppbvdse4.apps.googleusercontent.com", clientSecret: "didI13YDMmcMhsGmOJgIK9W_");
+            bool _useCloudAppClientIDs = true;
 
-            app.UseMicrosoftAccountAuthentication(
+            if (_useCloudAppClientIDs)
+            {
+                app.UseMicrosoftAccountAuthentication(
                 clientId: "0000000044126351",
                 clientSecret: "jAZpCRiJKg5gQRxFxvYBtpy5rdopTlnW");
 
-            //app.UseMicrosoftAccountAuthentication(
-            //   clientId: "0000000048128D31",
-            //   clientSecret: "vUmQPTbhTIctdtAhjWN-kDWodsNuJcNo");
-
-  //          app.UseYahooAuthentication(
-  //consumerKey: "dj0yJmk9N3Q1SkZVUjZJY29LJmQ9WVdrOVRXVldTR3BTTm5NbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD04Zg--",
-  //consumerSecret: "f317e16a47112d7ac2831b0c43cac572e1e7916f");
-            
- //           app.UseYahooAuthentication(
- //consumerKey: "dj0yJmk9cHd5M3pHeGJsWGJpJmQ9WVdrOVMyeE9VWEJrTjJFbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1lNA--",
- //consumerSecret: "0613308963b76ffee39299c08894d9f5aff427b5");
-            //var FbFriends = new FacebookAuthenticationOptions();
-
-            //FbFriends.SignInAsAuthenticationType = DefaultAuthenticationTypes.ExternalCookie;
-            app.UseFacebookAuthentication(
+                app.UseFacebookAuthentication(
                appId: "1526133034266985",
                appSecret: "1bbb93665abf17446745e9a1f1e5faa3");
-            //app.UseFacebookAuthentication(
-            //  appId: "271092676424493",
-            //  appSecret: "6257d28e03a066d87fa527774d854b87");
-            
-           // app.UseGoogleAuthentication();
 
-            var yahooAuthenticationOptions = new YahooAuthenticationOptions()
+                var yahooAuthenticationOptions = new YahooAuthenticationOptions()
+                {
+                    ConsumerKey = "dj0yJmk9TGVaNVhSTFdhU205JmQ9WVdrOVMyeE9VWEJrTjJFbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD00Mw--",
+                    ConsumerSecret = "1cdd4a7d9e35b684e12a4c87c45bbe83b1359133",
+                };
+                app.UseYahooAuthentication(yahooAuthenticationOptions);
+
+                var googleOAuth2AuthenticationOptions = new GoogleOAuth2AuthenticationOptions
+                {
+                    ClientId = "773164954343-mn2gou556p3oj9m49nh9j6i2j9qqh3da.apps.googleusercontent.com",
+                    ClientSecret = "3m3S31nHlSJ-buxLG6b3KnkI",
+                };
+                app.UseGoogleAuthentication(googleOAuth2AuthenticationOptions);
+            }
+            else
             {
-                ConsumerKey = "dj0yJmk9TGVaNVhSTFdhU205JmQ9WVdrOVMyeE9VWEJrTjJFbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD00Mw--",
-                ConsumerSecret = "1cdd4a7d9e35b684e12a4c87c45bbe83b1359133",
-            };
+                //R8R.wiki IDs
 
-            app.UseYahooAuthentication(yahooAuthenticationOptions);
+                app.UseMicrosoftAccountAuthentication(
+               clientId: "0000000048128D31",
+               clientSecret: "vUmQPTbhTIctdtAhjWN-kDWodsNuJcNo");
 
-           
+                app.UseFacebookAuthentication(
+               appId: "271092676424493",
+               appSecret: "6257d28e03a066d87fa527774d854b87");
 
-            var googleOAuth2AuthenticationOptions = new GoogleOAuth2AuthenticationOptions
-            {
-                ClientId = "773164954343-mn2gou556p3oj9m49nh9j6i2j9qqh3da.apps.googleusercontent.com",
-                ClientSecret = "3m3S31nHlSJ-buxLG6b3KnkI",
-            };
-            app.UseGoogleAuthentication(googleOAuth2AuthenticationOptions);
+                var yahooAuthenticationOptions = new YahooAuthenticationOptions()
+                {
+                    ConsumerKey = "dj0yJmk9OEp2RlhPSkVMOE80JmQ9WVdrOVR6WjNZVTFJTXpBbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD04NA—",
+                    ConsumerSecret = "34c4dde389ff43a49e4e91e205e744e5d874a85a",
+                };
+                app.UseYahooAuthentication(yahooAuthenticationOptions);
 
-            
+                var googleOAuth2AuthenticationOptions = new GoogleOAuth2AuthenticationOptions
+                {
+                    ClientId = "1072934350544-dl5fr7svaedmn33tms38a89kppbvdse4.apps.googleusercontent.com",
+                    ClientSecret = "didI13YDMmcMhsGmOJgIK9W_",
+                };
+                app.UseGoogleAuthentication(googleOAuth2AuthenticationOptions);
+
+            }
+
         }
     }
 }
